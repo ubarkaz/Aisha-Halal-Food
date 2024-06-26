6c84f59e2f056fd12343c0fa8274d508599f30d8
ubarkun <kasoziubar97@gmail.com>
2024-06-26 22:00:02 +0100

    made us of scrollReveal and swiper to improve UI

--- script.js
+++ script.js
@@ -37,3 +37,51 @@ ScrollReveal().reveal(".header_content .header_btn", {
     ...ScrollRevealOption,
     delay:1500,
 });
+
+ScrollReveal().reveal(".explore_image img", {
+    ...ScrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".explore_content .section_header", {
+    ...ScrollRevealOption,
+    delay:500,
+});
+ScrollReveal().reveal(".explore_content .section_description", {
+    ...ScrollRevealOption,
+    delay:1000,
+});
+ScrollReveal().reveal(".explore_content .explore_btn", {
+    ...ScrollRevealOption,
+    delay:1500,
+});
+ScrollReveal().reveal(".banner_card",{
+    ...ScrollRevealOption,
+    interval: 500,
+})
+ScrollReveal().reveal(".chef_image img",{
+    ...ScrollRevealOption,
+    origin: "right"
+})
+ScrollReveal().reveal(".chef_content .section_header",{
+    ...ScrollRevealOption,
+    delay: 500,
+})
+ScrollReveal().reveal(".chef_content .section_description",{
+    ...ScrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".chef_list li",{
+    ...ScrollRevealOption,
+    delay: 1500,
+    interval :500,
+});
+
+const swiper = new Swiper(".swiper",{
+    loop:true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },  
+})
